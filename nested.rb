62546afca79b53597439265125d3398f5602332b
Jonathan Snipe <jon.mike919@gmail.com>
2020-06-08 00:07:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -15,8 +14,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay] [:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +54,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages].first 
 end
 
 def adding_matz
@@ -79,8 +79,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+  
+programmer_hash # return the hash
+  
 end
 
 def changing_alan
@@ -102,6 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+ programmer_hash[:alan_kay][:known_for] = "GUI"
      
 end
 
@@ -123,3 +129,6 @@ def adding_to_dennis
      }
 
 end
+
+
+
